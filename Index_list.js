bbe37160491105acf0d1026ba734a0e9e143bd80
Haiyahhh <92042732+Haiyahhh@users.noreply.github.com>
2022-10-16 08:26:34 +0700

    Update Index_list.js

--- Index_list.js
+++ Index_list.js
@@ -1,5 +1,5 @@
 var author = document.getElementById("author");
-var bloglist = document.getElementById("dangminhhoang")
+var bloglist = document.getElementById("dangminhhoang");
 function showList(){
 
     if(bloglist.className == 'blog-list'){bloglist.className = 'blog-list show dropdown_menu-1'}
